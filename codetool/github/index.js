61b2753c7b7b10a6e9f87c64e70f39d3b5147d58
Alex Fomin <ShurickFomin@gmail.com>
2015-11-30 20:23:03 +0200

    add get git branches

--- codetool/github/index.js
+++ codetool/github/index.js
@@ -4,9 +4,21 @@ var koa = require('koa');
 var logger = require('koa-logger');
 var route = require('koa-route');
 var json = require('koa-json');
+var Github = require('github');
+var wrapper = require('co-github');
 var app = koa();
+var github = wrapper(new Github({
+    version: "3.0.0",
+    debug: true,
+    protocol: "https",
+    host: "api.github.com",
+    timeout: 5000
+}));
+github.authenticate({
+    type: "oauth",
+    token: process.env.TOKEN
+});
 
-var tp = new TP(process.env.TOKEN, "buildboard");
 
 
 app.use(json());
@@ -16,23 +28,19 @@ app.use(logger());
 
 app.use(route.get('/', capabilities));
 //app.use(route.get('/entities', capabilities));
-app.use(route.get('/users', users));
-app.use(route.get('/entities', entities));
+app.use(route.get('/branches', branches));
 //app.use(route.post('/entities/:id', capabilities));
 //app.use(route.get('/post/new', add));
 //app.use(route.get('/post/:id', show));
 //app.use(route.post('/post', create));
 
-app.listen(3333);
+app.listen(3334);
 
 
-function *users() {
-    this.body = {users: yield tp.getUsers()};
+function *branches() {
+    this.body = {branches: yield github.repos.getBranches({user: 'TargetProcess', repo: "buildboard2"})};
 }
 
-function *entities() {
-    this.body = {entities: yield tp.getEntities()};
-}
 
 function *capabilities() {
     this.body = yield {entities: []};
