fcee328d7530081ba0c5b84ab6d73de9359cfbf5
Alex Fomin <ShurickFomin@gmail.com>
2015-12-09 12:37:41 +0200

    Add link to tool-bootstrap

--- apps.js
+++ apps.js
@@ -1,8 +1,10 @@
 var execSync = require('child_process').execSync;
 
 execSync('pm2 kill');
-
+execSync('npm link', {cwd: 'tool-bootstrap'});
 ['pmtool/tp', 'codetool/github', 'buildtool/travis'].forEach(item=> {
+
     execSync("npm install", {cwd: item});
+    execSync("npm link tool-bootstrap");
     execSync("npm start", {cwd: item});
 });
