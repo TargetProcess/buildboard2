b6cc4d94d12c4d4a02014e85f53ba487dfaf1623
Alex Fomin <ShurickFomin@gmail.com>
2015-12-09 12:41:51 +0200

    Add 'npm install' for tool-bootstrap

--- apps.js
+++ apps.js
@@ -1,7 +1,10 @@
 var execSync = require('child_process').execSync;
 
 execSync('pm2 kill');
+
+execSync('npm install', {cwd: 'tool-bootstrap'});
 execSync('npm link', {cwd: 'tool-bootstrap'});
+
 ['pmtool/tp', 'codetool/github', 'buildtool/travis'].forEach(item=> {
 
     execSync("npm install", {cwd: item});
